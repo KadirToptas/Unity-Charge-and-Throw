6d5b967465a4832473de32bf4386e3e3573b8792
KadirToptas <kdrrtptss@gmail.com>
2024-06-25 16:11:22 +0300

    .

---
+++ Assets/Scripts/Cube_Controller.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Controller : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private Vector3 direction = Vector3.up;
+    [SerializeField] private float charge =0f;
+    [SerializeField] private float jumpForce =100f;
+    private bool _isGround = false;
+    private bool _isHolding = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !_isHolding && _isGround)
+        {
+            charge += 0.1f;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isHolding = true;
+        }
+
+        if (!_isGround)
+        {
+            charge = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isHolding)
+        {
+            rb.AddForce(direction* charge * jumpForce);
+            _isHolding = false;
+            _isGround = false;
+        }
+    }
+}
